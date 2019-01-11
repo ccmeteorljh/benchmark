436dafb1929e71358b1face944347eaa6294b35c
ccmeteorljh <33303691+ccmeteorljh@users.noreply.github.com>
2019-01-11 11:26:32 +0800

    Create dao.py

---
+++ libs/common/dao.py
@@ -0,0 +1,67 @@
+import MySQLdb
+
+import libs.common.exception as myexception
+
+
+class CommonDao(object):
+    """
+    定义数据库的常用操作
+    """
+    @staticmethod
+    def get_conn(**ksargs):
+        """ 连接数据库 """
+        host = ksargs['host'] if 'host' in ksargs else ''
+        user = ksargs['user'] if 'user' in ksargs else ''
+        port = ksargs['port'] if 'port' in ksargs else ''
+        passwd = ksargs['passwd'] if 'passwd' in ksargs else ''
+        db = ksargs['db'] if 'db' in ksargs else ''
+        charset = ksargs['charset'] if 'charset' in ksargs else ''
+        try:
+            conn = MySQLdb.connect(host=host, user=user, passwd=passwd,
+                               db=db, port=port, charset=charset)
+            cur = conn.cursor()
+
+        except MySQLdb.Error as e:
+            raise myexception.DBError('%s%s' % (e.message, 'ConnectError!'))
+        return conn, cur
+
+    @staticmethod
+    def exe_update(conn, cur, sql):
+        """ 更新语句，可执行Update，Insert语句 """
+        try:
+            sta = cur.execute(sql)
+            conn.commit()
+        except MySQLdb.Error as e:
+            conn.rollback()
+            raise myexception.DBError('%s%s' % (e.message, 'Update error!'))
+        return sta
+
+    @staticmethod
+    def exe_delete(conn, cur, sql):
+        """ 删除语句 """
+        try:
+            sta = cur.execute(sql)
+            conn.commit()
+        except MySQLdb.Error as e:
+            conn.rollback()
+            raise myexception.DBError('%s%s' % (e.message, "Delete error!"))
+        return sta
+
+    @staticmethod
+    def exe_query(cur, sql):
+        """ 查询语句 """
+        try:
+            cur.execute(sql)
+            result = cur.fetchall()
+        except MySQLdb.Error as e:
+            raise myexception.DBError('%s%s' % (e.message, "Select error!"))
+        return result
+
+    @staticmethod
+    def conn_close(conn, cur):
+        """ 关闭所有连接 """
+        try:
+            cur.close()
+            conn.close()
+        except MySQLdb.Error as e:
+            raise myexception.DBError('%s%s' % (e.message, "Close error!"))
