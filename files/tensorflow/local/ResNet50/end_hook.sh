dd2d300cc43eef7c6b068a392493f5cd2636bcd7
ccmeteorljh <33303691+ccmeteorljh@users.noreply.github.com>
2019-01-17 11:34:08 +0800

    Create end_hook.sh

---
+++ files/tensorflow/local/ResNet50/end_hook.sh
@@ -0,0 +1,14 @@
+export image_version=`cat /home/crim/run.log |grep 'my_job_image_version'|awk -F '=' '{print $2}'`
+export model=`cat /home/crim/run.log |grep 'my_job_model'|awk -F '=' '{print $2}'`
+export frame=`cat /home/crim/run.log |grep 'my_job_frame'|awk -F '=' '{print $2}'`
+job_id=`cat /home/crim/run.log |grep 'job_id'|cut -d ' ' -f5|cut -d '=' -f2`
+trainer_id=`cat /home/crim/run.log |grep 'PADDLE_TRAINER_ID'|cut -d'=' -f2`
+export log_name=$model'_'$job_id'_'$trainer_id
+training_role=`cat /home/crim/run.log |grep 'TRAINING_ROLE'|cut -d= -f2`
+tmp_name=${log_name}"_"${frame}"_"${image_version}
+rm /home/work/ljh_benchmark/logs/$tmp_name
+cat /home/crim/run.log >> /home/crim/$log_name
+cat /home/crim/end_hook.log >> /home/crim/$log_name
+cat /home/crim/gpu_use.log >> /home/crim/$log_name
+cp /home/crim/$log_name /home/work/ljh_benchmark/logs/$tmp_name
+python post_benchmark_log.py_holder
